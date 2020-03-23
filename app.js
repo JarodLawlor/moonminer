a059035cfa06796ae92e5e64ac2025a994c90162
Jarod Lawlor <jarodlaw93@gmail.com>
2020-03-23 09:24:58 -0600

    unfinished

---
+++ app.js
@@ -0,0 +1,35 @@
+let cheese = 0
+
+let upgrades = [
+{
+  name: "knife",
+increment: 1,
+inventory: 0,
+cost:50
+},
+{
+  name: "axe",
+increment: 5,
+inventory: 0,
+cost: 200
+},
+{
+name: "mouse",
+increment: 1,
+inventory: 0,
+cost: 500,
+interval: 3000
+},
+{
+name: "rover",
+increment: 10,
+inventory: 0,
+cost: 1000,
+interval: 3000
+}
+]
+
+function add(){
+
+
+}
